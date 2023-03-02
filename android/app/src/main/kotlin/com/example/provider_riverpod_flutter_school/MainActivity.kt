7ad9e2f7d66a4718f8a99270a136bd617ab81fd7
shinta <shintya2001@icloud.com>
2023-03-03 07:11:02 +0900

    cmmit

---
+++ android/app/src/main/kotlin/com/example/provider_riverpod_flutter_school/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provider_riverpod_flutter_school
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("MyHomePageStateをビルド");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // このconst大事
+            //constで「このWidgetは再描画しなくていいよ」って指示を出せる
+            const WidgetA(),
+            WidgetB(_counter),
+            WidgetC(_incrementCounter),
+          ],
+        ),
+      ),
+    ); // This trailing comma makes auto-formatting nicer fo
+  }
+}
+
+
+class WidgetA extends StatelessWidget {
+  const WidgetA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("WidgetAをビルド");
+    return const Text(
+      'You have pushed the button this many times:',
+    );
+  }
+}
+class WidgetB extends StatelessWidget {
+  const WidgetB(this.counter,{Key? key}) : super(key: key);
+
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    print("WidgetBをビルド");
+    return Text(
+      '$counter',
+      style: Theme.of(context).textTheme.headline4,
+    );
+  }
+}
+class WidgetC extends StatelessWidget {
+  const WidgetC(this.increment,{Key? key}) : super(key: key);
+  //ロジック型（処理型）の時はFunction
+  final Function increment;
+
+  @override
+  Widget build(BuildContext context) {
+    print("WidgetCをビルド");
+    return ElevatedButton(
+        onPressed: () {
+          increment();
+        },
+        child: Text("カウント")
+    );
+  }
+}
